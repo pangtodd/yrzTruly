3280d86fcc7436d4d099bf2afbc7b95ee87ea245
Todd Pangilinan <pang.todd@gmail.com>
2022-09-07 14:19:01 -0700

    established components folder/structure.

---
+++ yrz-truly/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Message type</h1>
+      <h3>message</h3>
+      <hr/>
+    </React.Fragment>
+  )
+}
+
+export default App;
