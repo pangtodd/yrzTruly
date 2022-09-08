f243cd894b41da1dc5c38c0e6b2594005e3423fb
Todd Pangilinan <pang.todd@gmail.com>
2022-09-08 13:30:24 -0700

    styled .img to control the height/width.

--- yrz-truly/src/components/Header.js
+++ yrz-truly/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import bannerImage from "./../img/yrzTruly.jpeg";
+import styled from 'styled-components';
 
+const BannerImgTamer = styled.img.attrs(props =>({
+  src: props.Img|| bannerImage,
+}))`
+  max-width: 80%;
+  height: auto;
+`;
 
 function Header(){
   return (
-    <img src={bannerImage} alt="old-timey banner" />
+    <BannerImgTamer>
+    </BannerImgTamer>
   );
 }
 
