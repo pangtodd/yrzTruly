3209a8299f4bb9574b49d99be2d969c2e09a92f9
Todd Pangilinan <pang.todd@gmail.com>
2022-09-08 14:21:35 -0700

    established initial NewGreetingForm.

---
+++ yrz-truly/src/components/NewGreetingForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewGreetingForm(props){
+  return(
+    <React.Fragment>
+      <h3>duh, I'm a form.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewGreetingForm;
