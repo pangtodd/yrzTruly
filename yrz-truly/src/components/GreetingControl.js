c5385d9cda6e55887db878ee154c2f9c8e9d55a7
Todd Pangilinan <pang.todd@gmail.com>
2022-09-08 14:24:41 -0700

    establish initial GreetingControl.js.

---
+++ yrz-truly/src/components/GreetingControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NewGreetingForm from './NewGreetingForm';
+import GreetingList from './GreetingList';
+
+class GreetingControl extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        {/* tbd */}
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default GreetingControl;
