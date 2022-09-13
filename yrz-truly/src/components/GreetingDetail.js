bdd7f7734cfe2b91fba4401456e2918cf5df5e96
Todd Pangilinan <pang.todd@gmail.com>
2022-09-13 11:58:27 -0700

    established initial GreetingDetail.js file.

---
+++ yrz-truly/src/components/GreetingDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GreetingDetail(props){
+  return(
+    <React.Fragment>
+      <h1>Greeting Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default GreetingDetail;
