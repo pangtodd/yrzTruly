ae244cabd19eeb0a4831b1133b4116d030278636
Todd Pangilinan <pang.todd@gmail.com>
2022-09-13 14:00:04 -0700

    update Greeting.js to accept handleChangingSelectedGreeting method.

--- yrz-truly/src/components/Greeting.js
+++ yrz-truly/src/components/Greeting.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 function Greeting(props){
   return(
     <React.Fragment>
-      <h3> Greetings! </h3>
-      <h3> A message for your beloved {props.relation} regarding their {props.occasion}:</h3>
-      <p>{props.message}</p>
-      <hr/>
+      <div onClick={()=> props.whenGreetingClicked(props.id)}>
+        <h3> Greetings! </h3>
+        <h3> A message for your beloved {props.relation} regarding their {props.occasion}:</h3>
+        <p>{props.message}</p>
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -15,7 +17,9 @@ function Greeting(props){
 Greeting.propTypes ={
   relation: PropTypes.string,
   message: PropTypes.string,
-  occasion: PropTypes.string
+  occasion: PropTypes.string,
+  id: PropTypes.string,
+  whenGreetingClicked: PropTypes.func
 }
 
 export default Greeting;
