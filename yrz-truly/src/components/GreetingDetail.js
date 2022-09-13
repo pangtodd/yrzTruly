c4c18f4627e5c6b12c3d036a4c6340636f2f5a69
Todd Pangilinan <pang.todd@gmail.com>
2022-09-13 14:14:29 -0700

    updated GreetingDetail to accept props/render selectedTicket.

--- yrz-truly/src/components/GreetingDetail.js
+++ yrz-truly/src/components/GreetingDetail.js
@@ -1,12 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function GreetingDetail(props){
+  const { greeting }= props;
+
   return(
     <React.Fragment>
       <h1>Greeting Detail</h1>
+      <h3>relationship to card recipeient: {greeting.relation}</h3>
+      <h3>ocassion for card: {greeting.occasion}</h3>
+      <h3>greeting: {greeting.message}</h3>
       <hr/>
     </React.Fragment>
   );
 }
+
+GreetingDetail.propTypes={
+  greeting:PropTypes.object
+};
 
 export default GreetingDetail;
