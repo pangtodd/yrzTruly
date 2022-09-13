0a3e429fc9e5466978816c364db45f96ea130c57
Todd Pangilinan <pang.todd@gmail.com>
2022-09-13 16:25:07 -0700

    update GreetingDetail to accept onClickingDelete prop.

--- yrz-truly/src/components/GreetingDetail.js
+++ yrz-truly/src/components/GreetingDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function GreetingDetail(props){
-  const { greeting }= props;
+  const { greeting, onClickingDelete }= props;
 
   return(
     <React.Fragment>
@@ -10,6 +10,7 @@ function GreetingDetail(props){
       <h3>relationship to card recipeient: {greeting.relation}</h3>
       <h3>ocassion for card: {greeting.occasion}</h3>
       <h3>greeting: {greeting.message}</h3>
+      <button onClick={()=>onClickingDelete(greeting.id)}>Delete greeting</button>
       <hr/>
     </React.Fragment>
   );
@@ -17,6 +18,7 @@ function GreetingDetail(props){
 
 GreetingDetail.propTypes={
   greeting:PropTypes.object
+  onClickingDelete: PropTypes.func,
 };
 
 export default GreetingDetail;
