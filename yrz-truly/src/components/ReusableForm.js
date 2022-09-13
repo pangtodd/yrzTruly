490b46fa8fbb2830b41526e36109337ff6c9bac1
Todd Pangilinan <pang.todd@gmail.com>
2022-09-13 16:46:13 -0700

    establish initial ReusableForm.js

---
+++ yrz-truly/src/components/ReusableForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return(
+
+  );
+}
+
+ReusableForm.propTypes={
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string,
+}
+
+export default ReusableForm;
