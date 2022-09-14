b96f434e37ff8f1f42a77b118c18148e037681df
Todd Pangilinan <pang.todd@gmail.com>
2022-09-14 13:49:27 -0700

    upated to use ReusableForm import.

--- yrz-truly/src/components/NewGreetingForm.js
+++ yrz-truly/src/components/NewGreetingForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
 
 function NewGreetingForm(props){
 
@@ -17,28 +18,10 @@ function NewGreetingForm(props){
 
   return(
     <React.Fragment>
-      <form onSubmit ={handleNewGreetingFormSubmission}>
-        <input 
-          type='text'
-          name='relation'
-          placeholder='who is this message for?'/>
-          <br></br>
-          <br></br>
-        <input
-          type='text'
-          name='occasion'
-          placeholder='occasion?'/>
-          <br></br>
-          <br></br>
-        <textarea
-          name= 'message'
-          placeholder='your message'/>
-          <br></br>
-          <br></br>
-        <button type='submit'>Submit message</button>
-        <br></br>
-        <br></br>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewGreetingFormSubmission}
+        buttonText="submit!"
+        />
     </React.Fragment>
   );
 }
