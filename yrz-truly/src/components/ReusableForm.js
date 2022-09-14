41199c6516ef8f30adfcf5d0e2dc2147f8c27f4c
Todd Pangilinan <pang.todd@gmail.com>
2022-09-14 13:48:32 -0700

    bugfix, corrected typos.

--- yrz-truly/src/components/ReusableForm.js
+++ yrz-truly/src/components/ReusableForm.js
@@ -3,7 +3,30 @@ import PropTypes from "prop-types";
 
 function ReusableForm(props) {
   return(
-
+    <React.Fragment>
+    <form onSubmit ={props.formSubmissionHandler}>
+      <input 
+        type='text'
+        name='relation'
+        placeholder='who is this message for?'/>
+        <br></br>
+        <br></br>
+      <input
+        type='text'
+        name='occasion'
+        placeholder='occasion?'/>
+        <br></br>
+        <br></br>
+      <textarea
+        name= 'message'
+        placeholder='your message'/>
+        <br></br>
+        <br></br>
+      <button type='submit'>Submit message</button>
+      <br></br>
+      <br></br>
+    </form>
+  </React.Fragment>
   );
 }
 
