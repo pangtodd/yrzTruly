7d8ba5abbd69ef54e16176cbede2b098b6d5079b
Todd Pangilinan <pang.todd@gmail.com>
2022-09-14 20:42:25 -0700

    added button and got detail to accept props.

--- yrz-truly/src/components/GreetingDetail.js
+++ yrz-truly/src/components/GreetingDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function GreetingDetail(props){
-  const { greeting, onClickingDelete }= props;
+  const { greeting, onClickingDelete, onClickingEdit }= props;
 
   return(
     <React.Fragment>
@@ -10,6 +10,9 @@ function GreetingDetail(props){
       <h3>relationship to card recipeient: {greeting.relation}</h3>
       <h3>ocassion for card: {greeting.occasion}</h3>
       <h3>greeting: {greeting.message}</h3>
+      <button onClick={ onClickingEdit}>Update greeting </button>
+      <br/>
+      <br/>
       <button onClick={()=>onClickingDelete(greeting.id)}>Delete greeting</button>
       <hr/>
     </React.Fragment>
@@ -19,6 +22,7 @@ function GreetingDetail(props){
 GreetingDetail.propTypes={
   greeting:PropTypes.object,
   onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
 };
 
 export default GreetingDetail;
