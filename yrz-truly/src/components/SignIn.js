27f31ca4d16836cdc54fcb3ca16f089b61c17977
Todd Pangilinan <pang.todd@gmail.com>
2022-09-22 11:57:10 -0700

    added sign up AND sign in forms.

--- yrz-truly/src/components/SignIn.js
+++ yrz-truly/src/components/SignIn.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { auth } from './../firebase.js';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
 function SignIn(){
 
   const [signUpSuccess, setSignUpSuccess]= useState(null);
+  const [signInSuccess, setSignInSuccess]= useState(null);
 
   function doSignUp(event){
     event.preventDefault();
@@ -17,6 +18,19 @@ function SignIn(){
       })
       .catch((error)=>{
         setSignUpSuccess(`There was a problem signing up: ${error.message}. :(`)
+      });
+  }
+
+  function doSignIn(event){
+    event.preventDefault();
+    const email = event.target.email.value;
+    cont password= event.target.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential)=>{
+        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}.`)
+      })
+      .catch((error)=>{
+        setSignInSuccess(`uh-oh. There was an error signing in: ${error.message}.`)
       });
   }
 
@@ -35,10 +49,29 @@ function SignIn(){
           type="password"
           name="password"
           placeholder="password" />
-           <br/>
+          <br/>
           <br/>
         <button type = "submit">Sign up</button>
       </form>
+      <h1>sign in</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input
+          type="text"
+          name="email"
+          placeholder="email" />
+          <br/>
+          <br/>
+        <input
+          type="password"
+          name="password"
+          placeholder="password" />
+          <br/>
+          <br/>
+        <button type = "submit">Sign in</button>
+      </form>
+
+
     </React.Fragment>
   );
 }
