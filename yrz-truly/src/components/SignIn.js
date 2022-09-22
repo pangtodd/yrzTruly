21263d811813eee66b74eb79d212e0d1960ea453
Todd Pangilinan <pang.todd@gmail.com>
2022-09-22 11:39:13 -0700

    create functional sign in form.

--- yrz-truly/src/components/SignIn.js
+++ yrz-truly/src/components/SignIn.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from './../firebase.js';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 function SignIn(){
+
+  const [signUpSuccess, setSignUpSuccess]= useState(null);
+
+  function doSignUp(event){
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential)=>{
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}.`)
+      })
+      .catch((error)=>{
+        setSignUpSuccess(`There was a problem signing up: ${error.message}. :(`)
+      });
+  }
+
   return(
-    <h1> Sign in, please! </h1>
+    <React.Fragment>
+      <h1>Sign up</h1>
+      {signUpSuccess}
+      <form onSubmit={doSignUp}>
+        <input
+          type="text"
+          name="email"
+          placeholder="email" />
+          <br/>
+          <br/>
+        <input
+          type="password"
+          name="password"
+          placeholder="password" />
+           <br/>
+          <br/>
+        <button type = "submit">Sign up</button>
+      </form>
+    </React.Fragment>
   );
 }
 
