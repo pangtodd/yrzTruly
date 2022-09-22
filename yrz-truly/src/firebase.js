7f20ed5b13c38561fd24dc5a8e18f8d388339d24
Todd Pangilinan <pang.todd@gmail.com>
2022-09-22 11:18:58 -0700

    update firebase.js to include auth.

--- yrz-truly/src/firebase.js
+++ yrz-truly/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig={
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,6 +15,7 @@ const firebaseConfig={
 
 const app= initializeApp(firebaseConfig);
 const db= getFirestore(app);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
-export default db;
+export {db, auth};
