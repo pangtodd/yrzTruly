82f939c644d8fbd864a98d4ffb5e964116636813
Todd Pangilinan <pang.todd@gmail.com>
2022-09-27 14:33:54 -0700

    updated EditGreetingForm to work with checkboxes.

--- yrz-truly/src/components/EditGreetingForm.js
+++ yrz-truly/src/components/EditGreetingForm.js
@@ -4,12 +4,24 @@ import PropTypes from "prop-types";
 
 function EditGreetingForm(props){
   const { greeting } =props;
+  
+  function handleCheckboxValues(checkboxType){
+    const checkyboxes =document.getElementsByName(checkboxType);
+    let result = "";
+    for (var i =0; i< checkyboxes.length; i++){
+      console.log(checkyboxes[i].checked)
+      if (checkyboxes[i].checked){
+        result+= checkyboxes[i].value+", ";
+      }
+    }
+    return result.slice(0, -2);
+  }
 
   function handleEditGreetingFormSubmission(event){
     event.preventDefault();
     props.onEditGreeting({
-      relation: event.target.relation.value,
-      occasion: event.target.occasion.value,
+      relation: handleCheckboxValues("relation"),
+      occasion: handleCheckboxValues("occasion"),
       message: event.target.message.value,
       id: greeting.id
     });
