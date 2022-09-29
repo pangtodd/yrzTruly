dac84f9d811ed1ae2a3b29f49906331fc0a65036
Todd Pangilinan <pang.todd@gmail.com>
2022-09-29 11:13:14 -0700

    Dropdown.js functioning, working on refactoring.

--- yrz-truly/src/components/Dropdown.js
+++ yrz-truly/src/components/Dropdown.js
@@ -1,10 +1,26 @@
 import { useState } from "react";
 
-function Dropdown(props) {
-  
-  return (
-    
-  );
-}
+const DroppyDown = ()=> {
+  const [value, setValue]= useState("co-worker");
 
-export default Dropdown;
+  const handleChange= (event)=> {
+    setValue(event.target.value);
+  };
+
+  return(
+    <div>
+      <label>
+        who is this greeting for?
+        <select value={value} onChange={handleChange}>
+          <option value="co-worker">co-worker</option>
+          <option value="friend">friend</option>
+          <option value="family">family member</option>
+        </select>
+      </label>
+      <p>you selected {value}</p>
+    </div>
+  );
+};
+
+
+export default DroppyDown;
