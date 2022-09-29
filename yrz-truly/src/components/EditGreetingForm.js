aa1d853ae3864dba82b4b106a1d3cb9561245cab
Todd Pangilinan <pang.todd@gmail.com>
2022-09-29 11:55:02 -0700

    adjusted text for rendering.

--- yrz-truly/src/components/EditGreetingForm.js
+++ yrz-truly/src/components/EditGreetingForm.js
@@ -9,7 +9,6 @@ function EditGreetingForm(props){
     const checkyboxes =document.getElementsByName(checkboxType);
     let result = "";
     for (var i =0; i< checkyboxes.length; i++){
-      console.log(checkyboxes[i].checked)
       if (checkyboxes[i].checked){
         result+= checkyboxes[i].value+", ";
       }
