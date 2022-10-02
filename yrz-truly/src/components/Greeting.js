0b7700e524dd5ef3edee378a30d29af9815d6398
Todd Pangilinan <pang.todd@gmail.com>
2022-10-02 16:22:13 -0700

    refactored to display data out of Firebase correctly.

--- yrz-truly/src/components/Greeting.js
+++ yrz-truly/src/components/Greeting.js
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Greeting(props){
+  const relationString = props.relation.toString().replaceAll(",",", ");
+  const occasionString = props.occasion.toString().replaceAll(",",", ");
   return(
     <React.Fragment>
       <div onClick={()=> props.whenGreetingClicked(props.id)}>
-        <h3> Greetings! </h3>
-        <h3> A message for your beloved {props.relation} regarding their {props.occasion}:</h3>
+        <h3> Audience: {relationString} </h3>
+        <h3> Occasion: {occasionString}</h3>
         <p>{props.message}</p>
         <hr/>
       </div>
