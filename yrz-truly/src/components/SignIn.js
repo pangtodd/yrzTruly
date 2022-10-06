1814504b72d2599481ba9773a35977c1ceac7c0f
Todd Pangilinan <pang.todd@gmail.com>
2022-10-06 15:27:45 -0700

    updated import and routing to allow for context. We'll see if works.

--- yrz-truly/src/components/SignIn.js
+++ yrz-truly/src/components/SignIn.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { auth } from './../firebase.js';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, getAuth } from "firebase/auth";
-
+// newBelow
+import firebase from "firebase";
+import { AuthContext } from "../Firebase/context";
 
 function SignIn(){
-
+  const { user } = userContext(AuthContext);
   const [signUpSuccess, setSignUpSuccess]= useState(null);
   const [signInSuccess, setSignInSuccess]= useState(null);
   const [signOutSuccess, setSignOutSuccess]= useState(null);
