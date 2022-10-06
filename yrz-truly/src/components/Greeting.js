70f7c3c683eeed93825590b94bb8fc4066ef434b
Todd Pangilinan <pang.todd@gmail.com>
2022-10-05 18:26:08 -0700

    minor change to greeting detail rendering.

--- yrz-truly/src/components/Greeting.js
+++ yrz-truly/src/components/Greeting.js
@@ -7,9 +7,11 @@ function Greeting(props){
   return(
     <React.Fragment>
       <div onClick={()=> props.whenGreetingClicked(props.id)}>
-        <h3> Audience: {relationString} </h3>
-        <h3> Occasion: {occasionString}</h3>
-        <p>{props.message}</p>
+        <p> Audience: {relationString}</p>
+        <p> Occasion: {occasionString}</p>
+        <h3>{props.message}</h3>
+        <br></br>
+        <p> author: {props.author}</p>
         <hr/>
       </div>
     </React.Fragment>
@@ -20,6 +22,8 @@ Greeting.propTypes ={
   relation: PropTypes.array,
   message: PropTypes.string,
   occasion: PropTypes.array,
+  author: PropTypes.string,
+
   id: PropTypes.string,
   whenGreetingClicked: PropTypes.func
 }
