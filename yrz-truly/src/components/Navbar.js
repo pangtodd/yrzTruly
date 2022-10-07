4078abc5b26221437c3a6d9074915f4c9bfc2892
Todd Pangilinan <pang.todd@gmail.com>
2022-10-07 12:10:55 -0700

    updated Navbar to render dynamically for unathenticated and authented users.

--- yrz-truly/src/components/Navbar.js
+++ yrz-truly/src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React, { useContext } from "react";
 import { auth } from "../firebase";
-import { Link } from "react-router-dom";
+import { Link, button } from "react-router-dom";
 import { AuthContext } from "../Firebase/context.js";
+
 
 function Navbar(){
   const { user } = useContext(AuthContext);
-  if (user == null){
     return(
       <React.Fragment>
-      <Link to ="/sign-in"> Sign In </Link>
-      <Link to= "/"> home </Link>
+        {user && <p>currently signed in as {user.displayName}</p>}
+        {user == null && 
+          <Link to= "/sign-in"> 
+            <button type="button"> Sign In </button>
+          </Link>}
+        {user && 
+          <Link to= "/sign-in"> 
+          <button type="button"> Sign out </button>
+        </Link>}
+        <Link to= "/"> 
+            <button type="button"> home</button>
+        </Link>
       </React.Fragment>
     )
-    } else {
-      return(
-        <React.Fragment>
-          <p>currently signed in as {user.displayName}</p>
-          <Link to= "/sign-in">Sign out</Link>
-          <br></br>
-          <Link to= "/"> home </Link>
-        </React.Fragment>
-      )
-    }
 
 }
 export default Navbar
