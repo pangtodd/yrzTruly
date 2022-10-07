bf8918869038cd904f16c4560fb7278d9ffefa18
Todd Pangilinan <pang.todd@gmail.com>
2022-10-07 13:09:08 -0700

    added conditional rending depending on if a user is logged in or now.

--- yrz-truly/src/components/Greeting.js
+++ yrz-truly/src/components/Greeting.js
@@ -7,7 +7,19 @@ function Greeting(props){
   const relationString = props.relation.toString().replaceAll(",",", ");
   const occasionString = props.occasion.toString().replaceAll(",",", ");
 
-  
+  if (user == null){
+    return(
+      <React.Fragment>
+        <div >
+          <p> Audience: {relationString}</p>
+          <p> Occasion: {occasionString}</p>
+          <h3>{props.message}</h3>
+          <br></br>
+          <p> author: {props.author}</p>
+          <hr/>
+        </div>
+      </React.Fragment>)
+  } else {
   return(
     <React.Fragment>
       <div >
@@ -21,6 +33,7 @@ function Greeting(props){
       </div>
     </React.Fragment>
   );
+}
 }
 
 Greeting.propTypes ={
