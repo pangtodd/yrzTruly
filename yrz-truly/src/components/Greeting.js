f437fba51416e74e656e13539aaa9e9fa263f93e
Todd Pangilinan <pang.todd@gmail.com>
2022-10-07 11:05:14 -0700

    added conditional rendering of edit button- only appears if author name matches currently logged in user's displayName.

--- yrz-truly/src/components/Greeting.js
+++ yrz-truly/src/components/Greeting.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import { AuthContext } from "../Firebase/context.js";
 
 function Greeting(props){
+  const { user } = useContext(AuthContext);
   const relationString = props.relation.toString().replaceAll(",",", ");
   const occasionString = props.occasion.toString().replaceAll(",",", ");
+
+  
   return(
     <React.Fragment>
-      <div onClick={()=> props.whenGreetingClicked(props.id)}>
+      <div >
         <p> Audience: {relationString}</p>
         <p> Occasion: {occasionString}</p>
         <h3>{props.message}</h3>
         <br></br>
         <p> author: {props.author}</p>
+        {props.author == user.displayName && <button onClick={()=> props.whenGreetingClicked(props.id)}>edit</button>}
         <hr/>
       </div>
     </React.Fragment>
