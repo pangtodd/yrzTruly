2fe1d28baf4ee80363bc751744a706a026639a71
Todd Pangilinan <pang.todd@gmail.com>
2022-10-07 23:02:00 -0700

    added buttons which appear when initial message is revealed.

--- yrz-truly/src/components/LandingPage.js
+++ yrz-truly/src/components/LandingPage.js
@@ -2,10 +2,13 @@ import React from "react";
 import DroppyDown from "./Dropdown";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
+import GreetingControl from './GreetingControl';
+import {useState} from 'react';
 
 const TestFonty = styled.h1`
   font-family: Garamond, serif;
 `;
+
 const relation = [
   { label: 'co-worker', value: 'co-worker' },
   { label: 'friend', value: 'friend' },
@@ -22,8 +25,9 @@ const occasion= [
   {label:"sympathy", value:"sympathy"},
 ]
 
-
 function LandingPage(props){
+
+  const [isShown, setIsShown]= useState(false);
 
   const filteredGreeting= function(inputOccasion, inputRelation){
     const newBigArray= props.greetingList.filter(greeting=>{return greeting.occasion.includes(inputOccasion)});
@@ -36,6 +40,7 @@ function LandingPage(props){
     const selectOccasionElement = document.getElementById('occasion').value;
     const selectRelationElement = document.getElementById('relation').value;
     const quote = filteredGreeting(selectOccasionElement,selectRelationElement);
+    setIsShown(true);
     document.getElementById('output').innerHTML = quote.message;
   };
 
@@ -50,11 +55,18 @@ function LandingPage(props){
         <br></br>
         <h3>what is the occasion?</h3>
         <DroppyDown options={occasion} label= {"occasion"}/>
+        <br></br>
         <button type = 'submit'>Get message!</button>
       </form>
       <TestFonty>
         <div>
           <h1 id="output"></h1>
+          {isShown && (
+          <div>
+          <button onClick={handleGreetingDisplay}>Meh. Try another one.</button>
+          <button onClick={props.onClickBroweList}>Browse full list of greetings.</button>
+          </div>
+          )}
         </div>
       </TestFonty>
     </React.Fragment>
@@ -62,7 +74,8 @@ function LandingPage(props){
 }
 
 LandingPage.propTypes={
-  greetingList: PropTypes.array
+  greetingList: PropTypes.array,
+  handleBrowseListClick: PropTypes.func
 }
 
 export default LandingPage;
