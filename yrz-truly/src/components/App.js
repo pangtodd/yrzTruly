65f1427ce4eaee09b5a97054215f50e70424b558
Todd Pangilinan <pang.todd@gmail.com>
2022-10-08 22:04:47 -0700

    experimeted with routing to the newGreetingForm, didn't work out. Reverted to old verison.

--- yrz-truly/src/components/App.js
+++ yrz-truly/src/components/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from "./../Firebase/context";
 import GreetingControl from "./GreetingControl";
 import GreetingList from "./GreetingList";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NewGreetingForm from "./NewGreetingForm";
 
 function App(){
   return (
