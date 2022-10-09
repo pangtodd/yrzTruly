3a37b8977424ac59dd798af605636e149fcdc930
Todd Pangilinan <pang.todd@gmail.com>
2022-10-08 22:07:13 -0700

    cleaned up imports no longer being used.

--- yrz-truly/src/components/Header.js
+++ yrz-truly/src/components/Header.js
@@ -1,8 +1,6 @@
 import React from "react";
 import bannerImage from "./../img/yrzTruly.jpeg";
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
-import Navbar from "./Navbar";
 
 const BannerImgTamer = styled.img.attrs(props =>({
   src: props.Img|| bannerImage,
