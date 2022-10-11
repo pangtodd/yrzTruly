39ae3f274498e5918fb439094bdbc54fe5b8afe0
Todd Pangilinan <pang.todd@gmail.com>
2022-10-11 11:47:33 -0700

    refactored to use filterMenu.

--- yrz-truly/src/components/GreetingList.js
+++ yrz-truly/src/components/GreetingList.js
@@ -1,30 +1,13 @@
 // import React from "react";
 import Greeting from "./Greeting";
 import PropTypes from "prop-types";
-import DroppyDown from "./Dropdown";
+import FilterMenu from "./FilterMenu"
 import {useState} from 'react';
 import { db, auth } from './../firebase.js';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-
-
-const relation = [
-  { label: 'co-worker', value: 'co-worker' },
-  { label: 'friend', value: 'friend' },
-  { label: 'family member', value: 'family member' },
-];
-
-const occasion= [
-  {label:"birthday", value:"birthday"},
-  {label:"congratulation", value:"congratulations"},
-  {label:"thank you", value:"thank you"},
-  {label:"get well", value:"get well"},
-  {label:"farewell", value:"farewell"},
-  {label:"welcome aboard", value:"welcome aboard"},
-  {label:"sympathy", value:"sympathy"},
-]
 
 function GreetingList(props){
 
@@ -42,10 +25,7 @@ function GreetingList(props){
     const selectRelationElement = document.getElementById('relation').value;
     const quoteCollection = filteredGreeting(selectOccasionElement,selectRelationElement);
     setQuotes(quoteCollection);
-    // setIsShown(true);
-    // document.getElementById('output').innerHTML = quote.message;
   };
-
 
   return(
     <React.Fragment>
@@ -58,24 +38,7 @@ function GreetingList(props){
         )
       }
       <form onSubmit = {handleGreetingDisplay}>
-        <Box sx={{ border: 3, borderColor: "#53868b", bgcolor: "inherit", color: 'primary.contrastText', flexGrow: 1 }}>
-          <Grid container spacing={2} background= 'primary.main' color='black'>
-            <Grid  item xs={6} md={4}>
-              <h3 align="center">Who is this for?</h3>
-              <div align ="center"><DroppyDown options={relation} label = {"relation"} /></div>
-            </Grid>
-            <Grid item xs={6} md={4}>
-              <h3 align="center">What is the occasion?</h3>
-              <div align ="center"><DroppyDown options={occasion} label = {"occasion"} /></div>
-              <br></br>
-            </Grid>
-            <Grid item xs={6} md={4}>
-              <br></br>
-              <br></br>
-              <div align="center"><button type = 'submit'>Get message!</button></div>
-            </Grid>
-          </Grid>
-        </Box>
+        <FilterMenu />
       </form>
       {quotes == null && 
         <div>
